f98242f0042f4d78c1c872a68d16e35273461773
Kushal <kushal.shit@geotechinfo.net>
2020-07-21 01:10:52 +0530

    Updates

---
+++ precache-manifest.c7843b6b271f949fd67394bf443ede89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427ef6f56fe12f1c6bebe52a14b572e7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bc150c66a9ecb535cab1",
+    "url": "/react-app/static/css/main.c95c3bd2.chunk.css"
+  },
+  {
+    "revision": "45de478a263230538d06",
+    "url": "/react-app/static/js/2.5db3ca9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.5db3ca9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc150c66a9ecb535cab1",
+    "url": "/react-app/static/js/main.a45efb8b.chunk.js"
+  },
+  {
+    "revision": "bc8126401b0d9bad4e61",
+    "url": "/react-app/static/js/runtime-main.d5cda9bc.js"
+  },
+  {
+    "revision": "63ab054f57a74cf1eb0b971c82152e51",
+    "url": "/react-app/static/media/card-top.63ab054f.jpg"
+  }
+]);
